7a26c10d6e6e111a6aaa039df4b31a4288c15844
宮副大佑 <miyazoedaisuke@miyafukudaisukenoMacBook-Pro.local>
2023-06-15 10:08:54 +0900

    first

---
+++ pages/browse.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="grid md:grid-cols-4 gap-3 py-10 px-6">
+    <div class="md:col-span-1 py-6 px-6 bg-teal-700 rounded-xl">
+      Left
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="py-20 px-6 bg-teal-700 text-center">
+    <h1 class="mb-12 text-5xl text-white">Find a job anywhere</h1>
+
+    <NuxtLink to="/browse" class="inline-block py-4 px-8 bg-teal-900 text-white rounded-xl">Browse</NuxtLink>
+  </div>
+
+  <div class="py-10 px-6">
+    <h2 class="mb-8 text-2xl text-center">Newest jobs</h2>
+
+    <div class="space-y-4">
+      <div class="p-6 flex items-center justify-between bg-gray-100 rounded-xl">
+        <div>
+          <h3 class="mb-2 text-xl font-semibold">The job position</h3>
+          <p class="text-gray-600">The company name</p>
+        </div>
+
+        <div>
+          <p class="mb-2">Worldwide</p>
+          <p>$90k-120k</p>
+        </div>
+
+        <div>
+          <p>Posted Dec. 1. 2022</p>
+        </div>
+
+        <div>
+          <NuxtLink to="/browse/1" class="py-4 px-6 bg-teal-700 text-white rounded-xl">Detail</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
